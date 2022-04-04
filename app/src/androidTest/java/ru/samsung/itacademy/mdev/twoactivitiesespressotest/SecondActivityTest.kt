0b819d2501684c0626b42059f05250145ebd33c1
ky3he4ik <misha10311@yandex.ru>
2022-04-04 11:58:56 +0300

    init

---
+++ app/src/androidTest/java/ru/samsung/itacademy/mdev/twoactivitiesespressotest/SecondActivityTest.kt
@@ -0,0 +1,44 @@
+package ru.samsung.itacademy.mdev.twoactivitiesespressotest
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.filters.LargeTest
+import androidx.test.runner.AndroidJUnit4
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class SecondActivityTest {
+    @Rule
+    @JvmField
+    var activityRule = activityScenarioRule<MainActivity>()
+
+    @Before
+    fun setUp() {
+        Intents.init()
+    }
+
+    @After
+    fun tearDown() {
+        Intents.release()
+    }
+
+    @Test
+    fun sendMessage() {
+        onView(withId(R.id.button_main)).perform(ViewActions.click())
+        onView(withId(R.id.text_message)).check(matches(ViewMatchers.withText("")))
+        onView(withId(R.id.editText_second)).perform(ViewActions.typeText("dsa"))
+        onView(withId(R.id.button_second)).perform(ViewActions.click())
+        onView(withId(R.id.text_message_reply)).check(matches(ViewMatchers.withText("dsa")))
+    }
+}
